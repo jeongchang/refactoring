106349d097710e253561cc444249c5cce44a2f31
zpro <zepiro00@gmail.com>
2021-08-17 15:22:18 +0900

    extraction switch from statement function

--- test.js
+++ test.js
@@ -43,3 +43,25 @@ function statement(invoice, plays){
     result +='적립 포인트: ${volumeCredits}점\n';
     return result;
 }
+
+function amountFor(perf, play){ //값이 바뀌지 않는 변수는 매개변수로 전달
+    let thisAmount = 0;         //변수를 초기화 하는 코드
+    switch(play.type){
+        case "tragedy": //비극
+            thisAmount = 40000;
+            if(perf.audience>30){
+                thisAmount += 1000 * (perf.audience -30);
+            }
+            break;
+        case "comedy": //희극
+            thisAmount = 30000;
+            if(perf.audience >20){
+                thisAmount += 10000 + 500 * (perf.audience -20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error('알 수 없는 장르 : ${paly.type}');
+    }
+    return thisAmount;
+}
