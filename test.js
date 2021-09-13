c02d228e53a800fff3c09a109febb79da08d593c
zpro <zepiro00@gmail.com>
2021-09-13 18:24:04 +0900

    insert backtic

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 function statement(invoice, plays){
     let totalAmount = 0;
     let volumeCredits = 0;
-    let result = '청구 내역 (고객명 : ${invoice.customer})\n';
-    const format = new Intl.NumberFormat("en-S", 
+    let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
+    const format = new Intl.NumberFormat("en-US", 
                                     { style: "currency", currency:"USD",
                                         minimumFractionDigits: 2}).format;
 
@@ -10,7 +10,8 @@ function statement(invoice, plays){
         //const play = plays[perf.playID];  //추출한 함수로 변경
         //const play = playFor(perf);     //우변을 함수로 추출 -> 변수 인라인 하기 적용
 
-        let thisAmount = amountFor(perf, playFor(perf)); // 추출한 함수를 이용
+        //let thisAmount = amountFor(perf, playFor(perf)); // 추출한 함수를 이용
+        let thisAmount = amountFor(perf); // 필요 없어진 매개변수 제거
             
         //포인트를 적립한다.
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -19,16 +20,17 @@ function statement(invoice, plays){
         if("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience /5);
 
         //청구 내역을 출력한다.
-        result += ' ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience}석\n';
+        result += `${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience}석\n`;
         totalAmount += thisAmount;
     }
 
-    result +='총액:${format(totalAmount/100)}\n';
-    result +='적립 포인트: ${volumeCredits}점\n';
+    result +=`총액:${format(totalAmount/100)}\n`;
+    result +=`적립 포인트: ${volumeCredits}점\n`;
     return result;
 }
 
-function amountFor(aPerformance, play){ // perf => aPerformance로 이름 변경
+//function amountFor(aPerformance, play){ // perf => aPerformance로 이름 변경
+function amountFor(aPerformance){ // 필요 없어진 매개변수 제거
     let result = 0;         // 명확한 이름으로 변경
     switch(playFor(aPerformance).type){ //play를 playFor()호출로 변경
         case "tragedy": //비극
@@ -45,11 +47,13 @@ function amountFor(aPerformance, play){ // perf => aPerformance로 이름 변경
             result += 300 * aPerformance.audience;
             break;
         default:
-            throw new Error('알 수 없는 장르 : ${playFor(aPerformance)}.type}');
+            throw new Error(`알 수 없는 장르 : ${playFor(aPerformance)}.type}`);
     }
-    return thisAmount;      //함수 안에서 값이 바뀌는 변수 반환
+    return result;      //함수 안에서 값이 바뀌는 변수 반환
 }
 
 function playFor(aPerformance){
     return plays[aPerformance.playID];
 }
+
+
