810b46a5b0c73a35573eac8e99ba36bcac59e745
zpro <zepiro00@gmail.com>
2021-09-14 08:23:37 +0900

    remove temp var

--- test.js
+++ test.js
@@ -2,25 +2,24 @@ function statement(invoice, plays){
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
+    
+    /*      임시 변수 삭제
     const format = new Intl.NumberFormat("en-US", 
                                     { style: "currency", currency:"USD",
                                         minimumFractionDigits: 2}).format;
+    */
 
     for(let perf of invoice.performances){
             
         //포인트를 적립한다.
-        //volumeCredits += Math.max(perf.audience - 30, 0); //함수로 추출
         volumeCredits += volumeCreditsFor(perf); //추출한 함수를 이용해 값을 누적
-        
-        //희극 관객 5명마다 추가 포인트를 제공한다.
-        //if("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience /5); //함수로 추출
 
         //청구 내역을 출력한다.
         result += `${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석\n`; //변수 인라인 적용
         totalAmount += amountFor(perf);
     }
 
-    result +=`총액:${format(totalAmount/100)}\n`;
+    result +=`총액:${format(totalAmount/100)}\n`;   //임시 변수였다. const format을 function format으로 대체
     result +=`적립 포인트: ${volumeCredits}점\n`;
     return result;
 }
@@ -60,4 +59,11 @@ function volumeCreditsFor(aPerformance){    //새로 추출한 함수
         result += Math.floor(aPerformance.audience /5);
     }
     return result;
+}
+
+function format(aNumber){
+    return new Intl.NumberFormat("en-US",
+    {
+        style:"currency", currency:"USD", minimumFractionDigits:2
+    }).format(aNumber);
 }
