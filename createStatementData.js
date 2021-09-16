7e7c923a5915efad30bbef9c52d2d1cfebea1b5c
zpro <zepiro00@gmail.com>
2021-09-16 19:10:39 +0900

    make class

--- createStatementData.js
+++ createStatementData.js
@@ -7,8 +7,9 @@ export default function createStatementData(injvoice, plays){
     return result;
 
     function enrichPerformance(aPerformance){
-        const result = Object.assign({}, aPerformance); //얕은 복사 수행
-        result.play = playFor(result);  // 중간데이터에 연극 정보를 저장.
+        const calculator = new PerformanceCalculator(aPerformance); //공연료 계산기 생성
+        const result = Object.assign({}, aPerformance); 
+        result.play = playFor(result); 
         result.amount = amountFor(result); 
         result.volumeCredits = volumeCreditsFor(result);
         return result;
@@ -19,9 +20,9 @@ export default function createStatementData(injvoice, plays){
         return plays[aPerformance.playID];
     }
 
-    function amountFor(aPerformance){ // 필요 없어진 매개변수 제거
-        let result = 0;         // 명확한 이름으로 변경
-        switch(aPerformance.play.type){ // 중간 데이터를 사용하도록 바꿈
+    function amountFor(aPerformance){ 
+        let result = 0;         
+        switch(aPerformance.play.type){ 
             case "tragedy": //비극
                 result = 40000;
                 if(aPerformance.audience>30){
@@ -38,13 +39,13 @@ export default function createStatementData(injvoice, plays){
             default:
                 throw new Error(`알 수 없는 장르 : ${aPerformance.play.type}`);
         }
-        return result;      //함수 안에서 값이 바뀌는 변수 반환
+        return result;
     }
 
     function volumeCreditsFor(aPerformance){
         let result = 0;
         result += Math.max(aPerformance.audience - 30, 0);
-        if( "comedy" === aPerformance.play.type){   //중간 데이터를 사용하도록 바꿈
+        if( "comedy" === aPerformance.play.type){ 
             result += Math.floor(aPerformance.audience /5);
         }
         return result;
@@ -53,9 +54,15 @@ export default function createStatementData(injvoice, plays){
     function totalAmount(data){ 
         return data.performances.reduce((total,p) => total + p.amount, 0);
     }
-    
+
     function totalVolumeCredits(data){
         return data.performances.reduce((total,p) => total + p.volumeCredits, 0);
     }
 
+}
+
+class PerformanceCalculator{    //공연료 계산기 클래스
+    constructor(aPerformance){
+        this.performance = aPerformance;
+    }
 }
