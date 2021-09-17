e3d9535d9882d902ff276600544ff7a1f23493eb
zpro <zepiro00@gmail.com>
2021-09-17 14:10:05 +0900

    amount() override

--- createStatementData.js
+++ createStatementData.js
@@ -52,25 +52,7 @@ class PerformanceCalculator{
     }
 
     get amount(){
-        let result = 0;         
-        switch(this.play.type){ 
-            case "tragedy": //비극
-                result = 40000;
-                if(this.performance.audience>30){
-                    result += 1000 * (this.performance.audience -30);
-                }
-                break;
-            case "comedy": //희극
-                result = 30000;
-                if(this.performance.audience >20){
-                    result += 10000 + 500 * (this.performance.audience -20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르 : ${this.play.type}`);
-        }
-        return result;
+        throw new Error(`서브클래스에서 처리하도록 설계되었습니다.`);
     }
 
     get volumeCredits(){
@@ -105,10 +87,12 @@ class TragedyCalculator extends PerformanceCalculator{
 }
 
 class ComedyCalculator extends PerformanceCalculator{
-    
+    get amount(){
+        let result = 30000;
+        if(this.performance.audience > 20){
+            result += 10000 + 500 * (this.performance.audience -20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
 }
-
-
-
-
-
